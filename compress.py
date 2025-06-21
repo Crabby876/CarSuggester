491681cd02cddc56aabea47baa919feed7122832
Doruk <142886360+Crabby876@users.noreply.github.com>
2025-06-21 17:45:32 +0200

    Delete compress.py

--- compress.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-# Original laden (besteht aus Tuple: (modell, spalten))
-modell, spalten = joblib.load('modell_rf.pkl')
-
-# Komprimiert speichern
-joblib.dump((modell, spalten), 'modell_komprimiert.joblib', compress=("xz",9))
-
-print("Modell und Spaltenliste wurden komprimiert als 'modell_komprimiert.joblib' gespeichert.")
