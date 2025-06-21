14095c690a06a30f25bb2d8b79f6308dad92dbbb
Doruk Güler <doriguler@gmail.com>
2025-06-21 17:06:34 +0200

    Initial commit ohne große Dateien

---
+++ compress.py
@@ -0,0 +1,9 @@
+import joblib
+
+# Original laden (besteht aus Tuple: (modell, spalten))
+modell, spalten = joblib.load('modell_rf.pkl')
+
+# Komprimiert speichern
+joblib.dump((modell, spalten), 'modell_komprimiert.joblib', compress=9)
+
+print("Modell und Spaltenliste wurden komprimiert als 'modell_komprimiert.joblib' gespeichert.")
