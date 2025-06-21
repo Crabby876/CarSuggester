0adc1d0988a2f49bcf8eabf4b515265dd6a42889
Doruk Güler <doriguler@gmail.com>
2025-06-21 17:37:28 +0200

    update app.py

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def index():
     return render_template("index.html", getriebe_typen=getriebe_typen, antrieb_typen=antrieb_typen, karosserie_typen=karosserie_typen, vorhersage=vorhersage, feedback=feedback)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
